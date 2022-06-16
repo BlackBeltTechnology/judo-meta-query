1e7303831f9c040868bb00750009e6c0d9ebe7ca
Bence Gelei <54545024+bencegelei@users.noreply.github.com>
2022-06-16 16:35:56 +0200

    JNG-3769 Improve logging (#10)
    * JNG-3769 Logging improved
    
    * JNG-3769 Update dependencies
    
    * JNG-3769 profile fixed

--- model-test/src/test/java/hu/blackbelt/judo/meta/query/runtime/QueryValidationTest.java
+++ model-test/src/test/java/hu/blackbelt/judo/meta/query/runtime/QueryValidationTest.java
@@ -2,27 +2,23 @@ package hu.blackbelt.judo.meta.query.runtime;
 
 import hu.blackbelt.epsilon.runtime.execution.api.Log;
 import hu.blackbelt.epsilon.runtime.execution.exceptions.EvlScriptExecutionException;
-import hu.blackbelt.epsilon.runtime.execution.impl.Slf4jLog;
+import hu.blackbelt.epsilon.runtime.execution.impl.BufferedSlf4jLogger;
 import hu.blackbelt.judo.meta.query.support.QueryModelResourceSupport;
-
+import lombok.extern.slf4j.Slf4j;
 import org.eclipse.emf.common.util.URI;
 import org.junit.jupiter.api.BeforeEach;
-import org.slf4j.LoggerFactory;
-import org.slf4j.Logger;
 
 import java.util.Collection;
 
 import static hu.blackbelt.judo.meta.query.support.QueryModelResourceSupport.queryModelResourceSupportBuilder;
 
-
+@Slf4j
 public class QueryValidationTest {
 
     QueryModelResourceSupport queryModelSupport;
-    
-    private static final Logger logger = LoggerFactory.getLogger(QueryValidationTest.class);
+
     private final String createdSourceModelName = "urn:Query.model";
 
-    private Log log = new Slf4jLog();
     private QueryModel queryModel;
     @BeforeEach
     void setUp() {
@@ -39,20 +35,20 @@ public class QueryValidationTest {
 
 
     private void runEpsilon (Collection<String> expectedErrors, Collection<String> expectedWarnings) throws Exception {
-        try {
-            QueryEpsilonValidator.validateQuery(log,
+        try (Log bufferedLog = new BufferedSlf4jLogger(log)) {
+            QueryEpsilonValidator.validateQuery(bufferedLog,
             		queryModel,
                     QueryEpsilonValidator.calculateQueryValidationScriptURI(),
                     expectedErrors,
                     expectedWarnings);
         } catch (EvlScriptExecutionException ex) {
-            logger.error("EVL failed", ex);
-            logger.error("\u001B[31m - expected errors: {}\u001B[0m", expectedErrors);
-            logger.error("\u001B[31m - unexpected errors: {}\u001B[0m", ex.getUnexpectedErrors());
-            logger.error("\u001B[31m - errors not found: {}\u001B[0m", ex.getErrorsNotFound());
-            logger.error("\u001B[33m - expected warnings: {}\u001B[0m", expectedWarnings);
-            logger.error("\u001B[33m - unexpected warnings: {}\u001B[0m", ex.getUnexpectedWarnings());
-            logger.error("\u001B[33m - warnings not found: {}\u001B[0m", ex.getWarningsNotFound());
+            log.error("EVL failed", ex);
+            log.error("\u001B[31m - expected errors: {}\u001B[0m", expectedErrors);
+            log.error("\u001B[31m - unexpected errors: {}\u001B[0m", ex.getUnexpectedErrors());
+            log.error("\u001B[31m - errors not found: {}\u001B[0m", ex.getErrorsNotFound());
+            log.error("\u001B[33m - expected warnings: {}\u001B[0m", expectedWarnings);
+            log.error("\u001B[33m - unexpected warnings: {}\u001B[0m", ex.getUnexpectedWarnings());
+            log.error("\u001B[33m - warnings not found: {}\u001B[0m", ex.getWarningsNotFound());
             throw ex;
         }
     }
