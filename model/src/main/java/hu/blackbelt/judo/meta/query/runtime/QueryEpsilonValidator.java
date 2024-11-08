43738e52dcb3bac0503685817f075ea0594dc6b6
Robert Csakany <csakany.robert@gmail.com>
2024-11-08 02:47:34 +0100

    Fix cache / parallel [skip ci]

--- model/src/main/java/hu/blackbelt/judo/meta/query/runtime/QueryEpsilonValidator.java
+++ model/src/main/java/hu/blackbelt/judo/meta/query/runtime/QueryEpsilonValidator.java
@@ -48,10 +48,19 @@ public class QueryEpsilonValidator {
     }
 
     public static void validateQuery(Logger log,
+                                     QueryModel queryModel,
+                                     URI scriptRoot,
+                                     Collection<String> expectedErrors,
+                                     Collection<String> expectedWarnings) throws ScriptExecutionException, URISyntaxException
+    {
+        validateQuery(log, queryModel, scriptRoot, expectedErrors, expectedWarnings, false);
+    }
+
+        public static void validateQuery(Logger log,
                                    QueryModel queryModel,
                                    URI scriptRoot,
                                    Collection<String> expectedErrors,
-                                   Collection<String> expectedWarnings) throws ScriptExecutionException, URISyntaxException
+                                   Collection<String> expectedWarnings, Boolean useCahe) throws ScriptExecutionException, URISyntaxException
     {
         ExecutionContext executionContext = executionContextBuilder()
                 .log(log)
@@ -64,6 +73,7 @@ public class QueryEpsilonValidator {
                                 .validateModel(false)
                                 .useCache(true)
                                 .resource(queryModel.getResource())
+                                .useCache(useCahe)
                                 .build()))
                 .injectContexts(singletonMap("queryUtils", new QueryUtils()))
                 .build();
@@ -78,6 +88,7 @@ public class QueryEpsilonValidator {
                                 .source(UriUtil.resolve("query.evl", scriptRoot))
                                 .expectedErrors(expectedErrors)
                                 .expectedWarnings(expectedWarnings)
+                                .parallel(true)
                                 .build());
 
             } finally {
